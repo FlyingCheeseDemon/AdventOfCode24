40302249f176d8bda556b3f9c5cefcb113ec0323
FlyingCheeseDemon <jakobbuehler@gmx.net>
2025-06-03 16:13:20 +0200

    first half of day 5

---
+++ 5a.py
@@ -0,0 +1,34 @@
+rules = []
+all_pages = []
+
+with open('5a-input.txt', 'r') as file:
+    for line in file:
+        if "|" in line:
+            rules.append([int(num) for num in line[:-1].split("|")])
+        elif "," in line:
+            all_pages.append([int(num) for num in line[:-1].split(",")])
+
+rule_dict = {}
+# let's make a dictionary that for each number tells me which numbers are *not* allowed to come after them
+for rule in rules:
+    if str(rule[1]) in rule_dict.keys():
+        rule_dict[str(rule[1])].append(rule[0])
+    else:
+        rule_dict[str(rule[1])] = [rule[0]]
+
+sum_of_middle_pages = 0
+for pages in all_pages:
+    is_valid = True
+    illegal_pages = []
+    for i,page in enumerate(pages):
+        if page in illegal_pages:
+            is_valid = False
+            break
+        if str(page) in rule_dict.keys():
+            illegal_pages += rule_dict[str(page)]
+    if is_valid:
+        length = len(pages)
+        middle = pages[int(length/2-0.5)]
+        sum_of_middle_pages+=middle
+
+print(sum_of_middle_pages)
