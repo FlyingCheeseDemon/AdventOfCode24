e5b1e734e15561a45aa548596d6906ad155c17a0
FlyingCheeseDemon <jakobbuehler@gmx.net>
2025-06-04 18:23:58 +0200

    day 7 solution

---
+++ 7a.py
@@ -0,0 +1,26 @@
+results = []
+numbers = []
+
+with open('7-input.txt', 'r') as file:
+    for line in file:
+        result, numbers_chars = line.split(":")
+        results.append(int(result))
+        numbers_chars = numbers_chars[1:-1].split(" ")
+        numbers_int = [int(num) for num in numbers_chars]
+        numbers.append(numbers_int)
+
+
+def does_it_work(target,current_value,remaining_numbers):
+    if len(remaining_numbers) == 0:
+        return current_value == target
+
+    test_multiply = does_it_work(target,current_value*remaining_numbers[0],remaining_numbers[1:])
+    test_add = does_it_work(target,current_value+remaining_numbers[0],remaining_numbers[1:])
+    return test_add or test_multiply
+
+total_sum = 0
+for result,number in zip(results,numbers):
+    if does_it_work(result,number[0],number[1:]):
+        total_sum += result
+
+print(total_sum)
